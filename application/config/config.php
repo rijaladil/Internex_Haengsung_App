3ba450b4c74a2ad75d2ef50d1e54cbc76a5ef500
RijalAdil <rijal23diy@gmail.com>
2020-03-18 15:47:05 +0700

    ngbvvbxb

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Internex_Daebaek_App/';
+// ipc$config['base_url'] = 'http://localhost/Internex_Daebaek_App/';
+$config['base_url'] = 'http://192.168.3.115/Internex_Daebaek_App/';
 /*
 |--------------------------------------------------------------------------
 | Index File
