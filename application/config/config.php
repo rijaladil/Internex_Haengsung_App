4ecee99de673d110412c6292fcd4307f30dfb9b2
Fadilla <jonggokowok@gmail.com>
2020-11-18 10:58:34 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -25,9 +25,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 
 
-$config['base_url'] = "http://".$_SERVER['HTTP_HOST'].str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME'])."/";
+// $config['base_url'] = "http://".$_SERVER['HTTP_HOST'].str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME'])."/";
 
-// $config['base_url'] = 'http://localhost/internex/daebaek/git/';
+$config['base_url'] = 'http://localhost/internex/haengsung/hns/';
 // $config['base_url'] = 'http://192.168.3.115/Internex_Daebaek_App/';
 
 /*
